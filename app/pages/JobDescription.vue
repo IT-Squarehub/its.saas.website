1b85efb7fa85575ca259d5c6cf32ae806d85eeba
KlarenceNicolasCatalan <112993918+KlarenceNicolasCatalan@users.noreply.github.com>
2025-01-13 16:15:27 +0800

    listed the list

--- app/pages/JobDescription.vue
+++ app/pages/JobDescription.vue
@@ -11,22 +11,40 @@
         </div>
 
         <!-- Qualifications -->
-        <div class=" w-1/2">
+        <div class=" w-1/2 pr-32">
             <p class=" text-4xl font-bold mb-5">
                 Qualifications:
             </p>
 
-            <p class=" mb-5">
-                Bachelor's degree in computer science, information technology, or a relative field. <br>
-                Proven experience as an ETL Developer or in a similar role. <br>
-                Strong knowledge of data integration concepts, ETL tools, and data warehousing technologies. <br>
-                Proficiency in ETL scripting and data manipulation languages. <br>
-                Solid understanding of data modeling and database design. <br>
-                Excellent problem-solving and analytical skills <br>
-                Effective communication and teamwork abilities. <br>
-                Knowledge of data security and privacy best practices. <br>
-                Familiarity with database systems (e.g., SQL Server, Oracle) and data warehousing platforms (e.g., Redshift, Snowflake). <br>   
-            </p>
+            <ul class=" mb-5 list-disc">
+                <li> 
+                    Bachelor's degree in computer science, information technology, or a relative field.
+                </li>
+                <li> 
+                    Proven experience as an ETL Developer or in a similar role.     
+                </li>
+                <li> 
+                    Strong knowledge of data integration concepts, ETL tools, and data warehousing technologies.    
+                </li>
+                <li> 
+                    Proficiency in ETL scripting and data manipulation languages.    
+                </li>
+                <li> 
+                    Solid understanding of data modeling and database design.     
+                </li>
+                <li> 
+                    Excellent problem-solving and analytical skills.    
+                </li>
+                <li> 
+                    Effective communication and teamwork abilities.     
+                </li>
+                <li> 
+                    Knowledge of data security and privacy best practices.    
+                </li>
+                <li> 
+                    Familiarity with database systems (e.g., SQL Server, Oracle) and data warehousing platforms (e.g., Redshift, Snowflake).     
+                </li>
+            </ul>
 
             <hr class=" bg-slate-400 pt-1 mr-8">
 
