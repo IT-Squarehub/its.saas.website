e851f7c3bd5860126645f6aa76691c1fdae31293
KlarenceNicolasCatalan <112993918+KlarenceNicolasCatalan@users.noreply.github.com>
2025-01-13 14:23:29 +0800

    Component naming change

---
+++ app/components/Career/CareerHero.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- Career Hero section -->
+        <div class=" bg-violet-400 px-32 pt-32 pb-44 max-[550px]:px-4 max-[1200px]:px-16">
+            <p class=" text-[80px] max-[550px]:text-4xl max-[1050px]:text-6xl">
+                Become part of our team!
+            </p>
+
+            <p class=" text-2xl pl-2 max-[550px]:text-lg max-[1050px]:pt-4 ">
+                Join ITSquarehub and become part of a dynamic team driving innovation in technology solutions.
+            </p>
+        </div>
+    </div>
+</template>
