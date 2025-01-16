2cff25d9b322e767ca6d3b08ec32b6128166e088
Mark Buduan <markiebudu@gmail.com>
2025-01-16 10:59:44 +0800

    made homepage responsive

--- app/components/Homepage/HomepageBanner.vue
+++ app/components/Homepage/HomepageBanner.vue
@@ -3,12 +3,15 @@
 
 <template>
     <div class="relative w-full bg-purple-300 text-white text-center py-32">
-        <img src="/assets/shapes.png" alt="Hero Image" class="absolute top-0 left-0 object-cover"/>
-        <div class="flex flex-col items-center justify-center gap-4 px-[30%] z-50">
-            <h1 class="text-6xl font-bold text-black">Your partner in software innovation</h1>
-            <p class="text-lg font-Inter font-thin text-black">Stay ahead of the competition with cutting-edge development, seamless integration, and collaborative tools from ITS SquareHub.</p>
+        <img src="/assets/shapes.png" alt="Hero Image"
+            class="sm:w-[18rem] lg:w-96 xl:w-auto w-56 absolute top-0 left-0 object-cover" />
+        <div class="flex flex-col items-center justify-center gap-4 px-[30%]">
+            <h1 class="md:text-7xl text-4xl font-bold text-black z-10">Your partner in software innovation</h1>
+            <p class="md:text-lg text-md font-Inter font-thin text-black z-10">Stay ahead of the competition with cutting-edge
+                development, seamless integration, and collaborative tools from ITS SquareHub.</p>
             <button class="bg-white text-black font-semibold px-4 py-2 rounded mt-4">Get Started</button>
         </div>
-        <img src="/assets/shapes.png" alt="Hero Image" class="absolute bottom-0 right-0 object-cover rotate-180" />
+        <img src="/assets/shapes.png" alt="Hero Image"
+            class="sm:w-[18rem] lg:w-96 xl:w-auto w-56 absolute bottom-0 right-0 object-cover rotate-180" />
     </div>
 </template>
--- app/components/Homepage/HomepageHero.vue
+++ app/components/Homepage/HomepageHero.vue
@@ -30,7 +30,7 @@ const { data: heroData, pending, error } = await useSanityQuery<HomepageHero>(qu
 
 </script>
 
-<template>
+<!-- <template>
  <div class="min-h-screen pt-28 text-center flex flex-col items-center justify-center bg-[url('/assets/net-background.png')] bg-cover bg-center">
     <div v-if="pending" class="loading-spinner">
       Loading...
@@ -55,6 +55,35 @@ const { data: heroData, pending, error } = await useSanityQuery<HomepageHero>(qu
         :src="heroData?.hero?.image ? urlFor(heroData.hero.image).width(1200).url() : '/assets/hero2.png'"
         :alt="heroData?.hero?.image?.alt || 'Hero image'"
       >
+    </div>
+  </div>
+</template> -->
+<template>
+  <div
+    class="min-h-screen pt-16 md:pt-28 text-center flex flex-col items-center justify-center bg-[url('/assets/net-background.png')] bg-cover bg-center px-10 lg:px-5">
+    <div v-if="pending" class="loading-spinner">
+      Loading...
+    </div>
+
+    <div v-else-if="error" class="error-message">
+      Error loading content
+    </div>
+
+    <div v-else class="flex flex-col items-center justify-center font-InstrumentSans w-full">
+      <h1
+        class="text-5xl lg:text-[4rem] font-Poppins font-bold min-h-[3rem] md:min-h-[6rem] leading-tight w-full md:w-[30rem] lg:w-[58rem] mb-5 bg-gradient-to-t from-slate-800 to-purple-500 bg-clip-text text-transparent">
+        {{ heroData?.hero?.heading || 'Hero Title' }}
+      </h1>
+
+      <button
+        class="bg-purple-700 p-2 md:p-3 rounded-md font-normal text-white flex justify-center items-center gap-2 text-sm md:text-base">
+        <Icon name="mdi:eye" />
+        <p>Enquire Now</p>
+      </button>
+
+      <img class="w-full h-auto md:h-[40rem] object-contain"
+        :src="heroData?.hero?.image ? urlFor(heroData.hero.image).width(1200).url() : '/assets/hero2.png'"
+        :alt="heroData?.hero?.image?.alt || 'Hero image'">
     </div>
   </div>
 </template>
