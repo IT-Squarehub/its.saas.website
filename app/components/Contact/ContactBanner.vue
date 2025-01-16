fa655658fe7bbb5eddb91a75196a2370152ae972
marc-nacpil <marc.nacpil@icloud.com>
2025-01-16 13:50:28 +0800

    enhanced responsiveness

--- app/components/Contact/ContactBanner.vue
+++ app/components/Contact/ContactBanner.vue
@@ -2,13 +2,15 @@
 </script>
 
 <template>
-    <div class="relative w-full bg-purple-300 text-white text-center py-32">
-        <img src="/assets/shapes.png" alt="Hero Image" class="absolute top-0 left-0 object-cover hidden lg:block"/>
-        <div class="flex flex-col items-center justify-center gap-4 px-10 z-50">
-            <h1 class="text-6xl font-bold text-black lg:px-[50%] xl:px-[0%]">Got more questions?</h1>
-            <p class="text-lg font-Inter font-thin md:px-20 lg:px-[30%] xl:px-[32%] text-black">Get in touch with us by providing your contact details above. We’ll make sure to answer all your queries and for you to get to know us better!</p>
+    <div class="relative w-full bg-purple-300 text-white text-center lg:py-40 py-32">
+        <img src="/assets/shapes.png" alt="Hero Image"
+            class="sm:w-[18rem] lg:w-96 xl:w-auto w-56 absolute top-0 left-0 object-cover hidden md:block" />
+        <div class="flex flex-col items-center justify-center gap-4 px-[10%] md:px-[25%]">
+            <h1 class="text-5xl md:text-6xl font-bold text-black ">Got more questions?</h1>
+            <p class="text-base md:text-lg font-Inter font-thin md:px-10 text-black">Get in touch with us by providing your contact details above. We’ll make sure to answer all your queries and for you to get to know us better!</p>
             <button class="bg-white text-black font-semibold px-4 py-2 rounded mt-4">Ask More</button>
         </div>
-        <img src="/assets/shapes.png" alt="Hero Image" class="absolute bottom-0 right-0 object-cover rotate-180 hidden lg:block" />
+        <img src="/assets/shapes.png" alt="Hero Image"
+            class="sm:w-[18rem] lg:w-96 xl:w-auto w-56 absolute bottom-0 right-0 object-cover rotate-180 hidden md:block" />
     </div>
 </template>
--- app/components/Contact/ContactHeader.vue
+++ app/components/Contact/ContactHeader.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-    <div class="relative w-full bg-purple-300 text-white py-28 mt-20">
+    <div class="relative w-full bg-purple-300 text-white py-24 md:py-28 lg:py-36 mt-20">
         <div class="flex flex-col gap-5 justify-center px-10 lg:px-20">
-            <h1 class="text-5xl md:text-5xl lg:text-5xl xl:text-6xl md:w-[100%] lg:w-[70%] xl:w-[70%] font-black text-black ">Ready to Elevate Your HR Operations to the Next Level?</h1>
-            <p class="text-base font-Inter font-thin text-black">Transform your workplace with modern HR solutions designed for today's challenges</p>
+            <h1 class="text-5xl md:text-5xl lg:text-5xl xl:text-6xl md:w-[75%] lg:w-[70%] xl:w-[70%] font-black text-black ">Ready to Elevate Your HR Operations to the Next Level?</h1>
+            <p class="text-base font-Inter font-thin md:w-[60%] text-black">Transform your workplace with modern HR solutions designed for today's challenges</p>
         </div>
-        <img src="/assets/shapes.png" alt="Hero Image" class="absolute bottom-5 right-0 object-cover rotate-180 hidden lg:block"/>
+        <img src="/assets/shapes.png" alt="Hero Image" class="absolute bottom-0 md:w-[40%] lg:bottom-4 lg:w-[40%] right-0 object-cover rotate-180 hidden md:block"/>
     </div>
 </template>
