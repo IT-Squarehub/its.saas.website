7d4ee010ab39a550f371e8553afd8bea90ef0a59
Mark Buduan <markiebudu@gmail.com>
2025-01-17 12:59:56 +0800

    added Sanity fetch data on contact page

--- app/components/Contact/ContactBanner.vue
+++ app/components/Contact/ContactBanner.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+    interface BannerSection {
+        banner: {
+            heading: string
+            subtitle: string
+        }
+    }
+
+    const query = groq`*[_type == "contactUs"][0] {
+        banner {
+        heading,
+        subtitle
+        }
+    }`
+
+
+    // Fetch data from Sanity
+    const { data: bannerData } = await useSanityQuery<BannerSection>(query)
 </script>
 
 <template>
@@ -6,8 +23,8 @@
         <img src="/assets/shapes.png" alt="Hero Image"
             class="sm:w-[18rem] lg:w-96 xl:w-auto w-56 absolute top-0 left-0 object-cover hidden md:block" />
         <div class="flex flex-col items-center justify-center gap-4 px-[10%] md:px-[25%]">
-            <h1 class="text-5xl md:text-6xl font-bold text-black ">Got more questions?</h1>
-            <p class="text-base md:text-lg font-Inter font-thin md:px-10 text-black">Get in touch with us by providing your contact details above. We’ll make sure to answer all your queries and for you to get to know us better!</p>
+            <h1 class="text-5xl md:text-6xl font-bold text-black ">{{ bannerData?.banner.heading }}</h1>
+            <p class="text-base md:text-lg font-Inter font-thin md:px-10 text-black"> {{ bannerData?.banner.subtitle }}</p>
             <button class="bg-white text-black font-semibold px-4 py-2 rounded mt-4">Ask More</button>
         </div>
         <img src="/assets/shapes.png" alt="Hero Image"
--- app/components/Contact/ContactHeader.vue
+++ app/components/Contact/ContactHeader.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+    interface HeaderSection {
+        heading: string,
+        subtitle: string,
+    }
+
+    const query = groq`*[_type == "contactUs"][0] {
+            heading,
+            subtitle,
+        }`
+
+
+    // Fetch data from Sanity
+    const { data: headerData } = await useSanityQuery<HeaderSection>(query)
 </script>
 
 <template>
     <div class="relative w-full bg-purple-300 text-white py-24 md:py-28 lg:py-36 mt-20">
         <div class="flex flex-col gap-5 justify-center px-10 lg:px-20">
-            <h1 class="text-5xl md:text-5xl lg:text-5xl xl:text-6xl md:w-[75%] lg:w-[70%] xl:w-[70%] font-black text-black ">Ready to Elevate Your HR Operations to the Next Level?</h1>
-            <p class="text-base font-Inter font-thin md:w-[60%] text-black">Transform your workplace with modern HR solutions designed for today's challenges</p>
+            <h1 class="text-5xl md:text-5xl lg:text-5xl xl:text-6xl md:w-[75%] lg:w-[70%] xl:w-[70%] font-black text-black ">{{ headerData?.heading }}</h1>
+            <p class="text-base font-Inter font-thin md:w-[60%] text-black">{{ headerData?.subtitle }}</p>
         </div>
         <img src="/assets/shapes.png" alt="Hero Image" class="absolute bottom-0 md:w-[40%] lg:bottom-4 lg:w-[40%] right-0 object-cover rotate-180 hidden md:block"/>
     </div>
