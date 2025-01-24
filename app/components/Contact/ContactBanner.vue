7391eaedb510bae8b20ae0a84858ad40ea6b70ef
KlarenceNicolasCatalan <112993918+KlarenceNicolasCatalan@users.noreply.github.com>
2025-01-24 10:22:16 +0800

    added v-if to contactBanner

--- app/components/Contact/ContactBanner.vue
+++ app/components/Contact/ContactBanner.vue
@@ -34,9 +34,11 @@ onMounted(() => {
         <img src="/assets/shapes.png" alt="Hero Image" data-aos="fade-right"
             class="sm:w-[18rem] lg:w-96 xl:w-auto w-56 absolute top-0 left-0 object-cover hidden md:block" />
         <div class="flex flex-col items-center justify-center gap-4 px-[10%] md:px-[28%]">
-            <h1 class="text-5xl md:text-6xl font-bold text-black " data-aos="fade-up">{{ bannerData?.banner.heading }}
+            <h1 v-if="bannerData?.banner.heading != null"
+                class="text-5xl md:text-6xl font-bold text-black " data-aos="fade-up">{{ bannerData?.banner.heading }}
             </h1>
-            <p class="text-base md:text-lg font-Inter font-thin md:px-10 text-black" data-aos="fade-up"> {{
+            <p  v-if="bannerData?.banner.subtitle != null"
+                class="text-base md:text-lg font-Inter font-thin md:px-10 text-black" data-aos="fade-up"> {{
                 bannerData?.banner.subtitle }}</p>
             <NuxtLink :to="{ path: '/contact', hash: '#form' }"
                 
